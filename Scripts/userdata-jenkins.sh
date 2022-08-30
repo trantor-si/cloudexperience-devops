d141366ece10814586e947874100eb16bc3969de
trantor-si <luccostajr@gmail.com>
2022-08-30 19:19:33 -0300

    feat: :sparkles: source code for a process of CI/CD using jenkins and gitlab.

---
+++ Scripts/userdata-jenkins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install epel -y
+sudo yum install daemonize -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
